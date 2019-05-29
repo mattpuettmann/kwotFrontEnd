cc1ba4155d4db6e8e1759b2424730db14cfbb90d
Matthew Puettmann <mattpuettmann@Matthews-MacBook-Pro.local>
2019-05-29 12:44:59 -0600

    front end sanity check

--- kwot/src/AuthGateway/EditContainer/EditContainer.jsx
+++ kwot/src/AuthGateway/EditContainer/EditContainer.jsx
@@ -8,12 +8,50 @@ class EditContainer extends Component {
             email: ""
         }
     }
+
+    componentDidMount(){
+        this.setState({
+            username: this.props.username,
+            email: this.props.email
+        })
+    }
+    handleFormChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+    handleSubmit = (e) => {
+        console.log('heyoooooooooo');
+        e.preventDefault();
+        this.props.handleEdit(this.state);
+        console.log('handleEdit being hit!!')
+        console.log(this.props)
+    }
+    handleDelete = async (e) => {
+        e.preventDefault();
+        console.log("Deleting!");
+        try{
+            const result = await fetch(`http://localhost:8000/users/registration/me`, {
+                method: "DELETE",
+                credentials: "include"
+            })
+            const parsedResult = await result.json();
+            console.log(parsedResult)
+        }catch(err){
+            console.log(err);
+        }
+    }
+
+
+
+
     render(){
+        console.log(this.state)
         return <div>
             <h1>Edit page!!!!!</h1>
-            Username: <input type="text" name="username"/><br/>
-            Email: <input type="text" name="email"/><br/>
-            <input type="submit"/>
+            Username: <input onChange={this.handleFormChange} type="text" name="username" value={this.state.username} /><br/>
+            Email: <input onChange={this.handleFormChange} type="text" name="email" value={this.state.email}/><br/>
+            <button onClick={this.handleSubmit}>Submit Edits</button>
         </div>
     }
 }
--- kwot/src/UserContainer/UserContainer.jsx
+++ kwot/src/UserContainer/UserContainer.jsx
@@ -9,45 +9,13 @@ class UserContainer extends Component {
         }
     }
 
-    handleEdit = async (formData) => {
-        try{
-            const result = await fetch(`http://localhost:8000/users/registration/me`, {
-                method: "PUT",
-                credentials: 'include',
-                headers: {
-                    "Content-type": "application/json"
-                },
-                body: JSON.stringify(formData)
-            })
-            const parsedResult = await result.json();
-            console.log(parsedResult);
-            console.log(formData)
-            await this.setState({
-                username: parsedResult.data.username,
-                email: parsedResult.data.email
-            })
-        }catch(err){
-            console.log(err);
-        }
-    }
-    showModal = () => {
-        this.setState({
-            showModal: !this.state.showModal
-        })
-    }
-
 
     render(){
         return <div>
             <h1>Logged in to {this.props.username}'s User Container!</h1>
-            {this.state.showModal ?
-            <EditContainer username={this.props.username} email={this.props.email} handleEdit={this.handleEdit}/>
-            :
-            null
-            }<br/>
-            <button onClick={this.showModal}>Edit</button>
-            <button>Delete</button>
+            <button>Add New Quote</button>
             <button onClick={this.props.handleLogout}>Logout</button>
+            <h3>Here are all of {this.props.username}'s quotes:</h3>
         </div>
     }
 }
