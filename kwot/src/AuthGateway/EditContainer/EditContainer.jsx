b8a1fddd90f666a174341b6ccb2be318c64f5604
Matthew Puettmann <mattpuettmann@Matthews-MacBook-Pro.local>
2019-05-30 14:39:23 -0600

    mvp

--- kwot/src/AuthGateway/EditContainer/EditContainer.jsx
+++ kwot/src/AuthGateway/EditContainer/EditContainer.jsx
@@ -4,55 +4,71 @@ class EditContainer extends Component {
     constructor(){
         super();
         this.state = {
-            username: "",
-            email: ""
+            id: "",
+            body: "",
+            attributed_to: "",
+            medium: ""
+        }
+    }
+    componentDidMount(){
+        console.log('it mounted!')
+        this.setState({
+            ...this.props.quote_to_edit
+
+        })
+        
+    }
+    fillForm = async (e) => {
+        e.preventDefault()
+        const response = await fetch('http://localhost:8000/api/v1/quotes', {
+            method: "GET",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        const parsedResponse = response.json()
+        console.log(parsedResponse)
+    }
+
+    handleEdit = async (e) => {
+        e.preventDefault();
+        console.log('edit button clicked')
+        console.log(this.state)
+        try{
+            const response = await fetch('http://localhost:8000/api/v1/quotes/' + this.state.id, {
+                method: "PUT",
+                credentials: "include",
+                body: JSON.stringify(this.state),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+                
+            })
+            const parsedResponse = await response.json()
+            console.log(parsedResponse)
+        }catch(err){
+            console.log(err)
         }
     }
 
-    componentDidMount(){
-        this.setState({
-            username: this.props.username,
-            email: this.props.email
-        })
-    }
+
     handleFormChange = (e) => {
+        e.preventDefault();
         this.setState({
+            ...this.props.quote_to_edit,
             [e.target.name]: e.target.value
         })
     }
-    handleSubmit = (e) => {
-        console.log('heyoooooooooo');
-        e.preventDefault();
-        this.props.handleEdit(this.state);
-        console.log('handleEdit being hit!!')
-        console.log(this.props)
-    }
-    handleDelete = async (e) => {
-        e.preventDefault();
-        console.log("Deleting!");
-        try{
-            const result = await fetch(`http://localhost:8000/users/registration/me`, {
-                method: "DELETE",
-                credentials: "include"
-            })
-            const parsedResult = await result.json();
-            console.log(parsedResult)
-        }catch(err){
-            console.log(err);
-        }
-    }
-
-
-
-
     render(){
         console.log(this.state)
-        return <div>
-            <h1>Edit page!!!!!</h1>
-            Username: <input onChange={this.handleFormChange} type="text" name="username" value={this.state.username} /><br/>
-            Email: <input onChange={this.handleFormChange} type="text" name="email" value={this.state.email}/><br/>
-            <button onClick={this.handleSubmit}>Submit Edits</button>
-        </div>
+        console.log(this.props.quote_to_edit)
+        return <form>
+            Body: <input type="text" name ="body" onChange={this.handleFormChange} value={this.state.body}/><br/>
+            Author: <input onChange={this.handleFormChange} type="text" name ="attributed_to" value={this.state.attributed_to}/><br/>
+            Medium: <input onChange={this.handleFormChange} type="text" name="medium" value={this.state.medium}/><br/>
+            <button onClick={this.handleEdit}>Submit Edits</button>
+        </form>
     }
 }
 
--- kwot/src/QuoteContainer/QuoteContainer.jsx
+++ kwot/src/QuoteContainer/QuoteContainer.jsx
@@ -1,10 +1,18 @@
 import React, {Component} from 'react';
+import EditContainer from '../AuthGateway/EditContainer/EditContainer';
 
 class QuoteContainer extends Component {
     constructor(){
         super();
         this.state = {
-            quotes: []
+            show_edit: false,
+            quotes: [],
+            quote_to_edit: {
+                id: null,
+                body: "",
+                attributed_to: "",
+                medium: ""
+            }
         }
     }
 
@@ -29,6 +37,8 @@ class QuoteContainer extends Component {
       }
     }
 
+
+
     handleDelete = async (id, e) => {
         e.preventDefault();
         console.log('delete button clicked')
@@ -43,9 +53,22 @@ class QuoteContainer extends Component {
             })
             const parsedResponse = await response.json();
             console.log(parsedResponse)
+            this.setState({
+                quotes: this.state.quotes.filter((quote, i) => quote.id !== id)
+            })
         }catch(err){
             console.log(err)
         }
+    }
+
+    showEdit = (quote, e) => {
+        e.preventDefault()
+        this.setState({
+            show_edit: true,
+            quote_to_edit: quote
+            
+        })
+
     }
 
     render(){
@@ -53,12 +76,22 @@ class QuoteContainer extends Component {
 
         return <div>
             <h5>All of the user's quotes go here.</h5>
+            {this.state.show_edit ?
+            <EditContainer quote_to_edit={this.state.quote_to_edit}/>
+            :
+            null
+            }
             {this.state.quotes.map((quote, index) => (
             <div key={quote.id}>
                 <p>{quote.body}</p>
                 <button onClick={this.handleDelete.bind(null, quote.id)}>DELETE</button>
+                <button onClick={this.showEdit.bind(null, quote)}>EDIT</button>
+                
+                
+                
             </div>
-    ))}
+            ))}
+            
         </div>
     }
 }
